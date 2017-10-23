d11cc927c9463832100fab4cebae508ee91bfcc7
mdatsev <martin.datsev.p@gmail.com>
2017-10-23 17:26:58 +0300

    added form to submit notes and listing them below for debug

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,4 +1,11 @@
 class NotesController < ApplicationController
+    #GET /notes
     def index
+        @all_notes = Note.all
+    end
+    #POST /notes
+    def create
+        Note.new({content: params[:content] , slug: "123"}).save
+        redirect_to "/notes"
     end
 end
