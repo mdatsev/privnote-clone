be145016b382364176ebdd6eb8be45955e36e3f5
mdatsev <martin.datsev.p@gmail.com>
2017-10-25 15:24:12 +0300

    added user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,23 @@
-class UsersController < ApplicationController
+class UsersController < ApplicationController  
+
   def new
+    @user = User.new 
   end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      #flash ...
+      redirect_to '/'
+    else
+      render 'new'
+    end   
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end 
+
 end
