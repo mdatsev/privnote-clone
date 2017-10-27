4a7fdeed776a099670169924d9a067368f312902
mdatsev <martin.datsev.p@gmail.com>
2017-10-27 20:11:07 +0300

    paypal button and user authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,7 +16,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find_by(id: params[:id])
+    if current_user != @user = User.find_by(id: params[:id])
+      redirect_to login_path
+    end
   end
 
   private
