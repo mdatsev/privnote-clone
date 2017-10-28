b0ff9fbdbc18578714262fa1bed8b91bbda73430
mdatsev <martin.datsev.p@gmail.com>
2017-10-28 14:31:19 +0300

    added pro column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171027120515) do
+ActiveRecord::Schema.define(version: 20171028112743) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20171027120515) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "remember_digest"
+    t.boolean "pro", default: false
   end
 
 end
