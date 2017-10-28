5afb4ae0c89395fd96f81a4902597296dbf37a0e
mdatsev <martin.datsev.p@gmail.com>
2017-10-28 16:51:13 +0300

    simplified user password handling

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,22 +1,12 @@
 class User < ActiveRecord::Base 
-    before_save :encrypt_password
-    after_save :clear_password
-    def encrypt_password
-      if password.present?
-        self.salt = BCrypt::Engine.generate_salt
-        self.encrypted_password= BCrypt::Engine.hash_secret(password, salt)
-      end
-    end
-    def clear_password
-      self.password = nil
-    end
     def self.authenticate(username_or_email, login_password)
       if EMAIL_REGEX.match(username_or_email)    
         user = User.find_by email: username_or_email
       else
         user = User.find_by username: username_or_email
       end
-      if user && user.match_password(login_password)
+      p BCrypt::Password.new(user.password_digest)
+      if user && BCrypt::Password.new(user.password_digest).is_password?(login_password)
         return user
       else
         return false
@@ -25,14 +15,12 @@ class User < ActiveRecord::Base
     def match_password(login_password)
       self.encrypted_password == BCrypt::Engine.hash_secret(login_password, salt)
     end
-    attr_accessor :password
     attr_accessor :remember_token
     EMAIL_REGEX = /\A\S+@\S+\.\S+\z/
     USERNAME_REGEX = /\A\w+\z/
     validates :username, presence: true, uniqueness: true,  format: USERNAME_REGEX
     validates :email, presence: true, uniqueness: true, format: EMAIL_REGEX
-    validates :password, confirmation: true
-    validates_length_of :password, in: 6..20, on: create
+    has_secure_password
     def User.new_token
       SecureRandom.urlsafe_base64
     end
